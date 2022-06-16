3de513cadaa2cca2e698ffcd495b52cbd3a1516c
Promise <976949689@qq.com>
2022-06-16 23:39:50 +0800

    2022年6月16日23:39:44

--- MybatisPlus/src/main/java/com/clearlove/version/MyBatisPlusConfig.java
+++ MybatisPlus/src/main/java/com/clearlove/version/MyBatisPlusConfig.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -38,5 +40,21 @@ public class MyBatisPlusConfig {
   @Bean
   public ISqlInjector sqlInjector() {
     return new LogicSqlInjector();
+  }
+
+  // sql执行效率插件
+  @Bean
+  // 设置dev test环境开启，保证开发效率
+  @Profile({"dev","test"})
+  public PerformanceInterceptor performanceInterceptor() {
+    PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+    // 设置sql执行的最大时间，如果超过了则不执行
+    // ms
+    // 在工作中，不允许用户等待
+    performanceInterceptor.setMaxTime(100);
+    // 是否格式化代码
+    performanceInterceptor.setFormat(true);
+
+    return performanceInterceptor;
   }
 }
